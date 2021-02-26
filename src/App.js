a1a244f909285db06b84cb948e698e87be05a45e
kumarkundan862 <kumarkundan862@gmail.com>
2021-02-26 12:55:10 +0530

    added styles to add button and name and phone

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import Header from "./Header";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <button>Add</button>
-        <div>
-          <span>name</span><br />
-          <span>phone</span>
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
         </div>
-        
       </div>
 
-        
+
     );
   }
 }
