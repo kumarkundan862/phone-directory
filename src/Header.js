e7fb646a6bd80af547301169d788afb308d130c0
kumarkundan862 <kumarkundan862@gmail.com>
2021-02-28 16:11:13 +0530

    Passed heading from parent App.js to Header component as props

--- src/Header.js
+++ src/Header.js
@@ -3,36 +3,28 @@ import React from 'react';
 import './Header.css';
 
 //this is functional component
-const Header = function(){
-
-    
-    // const headerStyle = {
-    //     textAlign: 'center',
-    //     padding: 20,
-    //     background: '#000',
-    //     color: '#fff',
-    //     textTransform: 'uppercase'
-    //  };
-     
+const Header = function(props){
+  
     return(
         <div className="header">
-          phone directory
+          {/* phone directory */}
+          {props.heading}
         </div>
     )
 }
 
 export default Header;
 
-//the functional component can be written in class component as well as shown below
+// //the functional component can be written in class component as well as shown below
 // class Header extends Component {
 //     reder(){
 //         return(
 //             <div className="header">
-//               phone directory
+//               {this.props.heading}
 //             </div>
 //         )
 //     }
 // }
-//
-//export default Header;
+
+// export default Header;
 
